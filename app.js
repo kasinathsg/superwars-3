99bac525b1c5509ff36fe5ebdb6f61d13eaaa35b
kasinathsg <108450699+kasinathsg@users.noreply.github.com>
2022-11-22 14:27:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+const PLAYERS = [
+    "Spiderman",
+    "Captain America",
+    "Wonderwoman",
+    "Popcorn",
+    "Gemwoman",
+    "Bolt",
+    "Antwoman",
+    "Mask",
+    "Tiger",
+    "Captain",
+    "Catwoman",
+    "Fish",
+    "Hulk",
+    "Ninja",
+    "Black Cat",
+    "Volverine",
+    "Thor",
+    "Slayer",
+    "Vader",
+    "Slingo"
+];
+
+// initialize players with image and strength
+const initPlayers = (players) => {
+    // Instead of forloop use Map method
+    // Code here
+    detailedPlayers= players.map((abc, i) => {
+        return{
+            name:abc,
+            strength:getRandomStrength(),
+            image:`images/super-${i+1}.png`,
+            type: i%2==0?"hero":"villain"
+        }
+    })
+    return detailedPlayers;
+}
+
+
+// getting random strength
+const getRandomStrength = () => {
+    return Math.ceil(Math.random() * 100);
+}
+
+// Build player template
+const buildPlayers = (players, type) => {
+    // Instead of using for loop
+    // Use chaining of Array methods - filter, map and join
+    // Type your code here
+    let valid=players.filter(playerData=>{
+        if (playerData.type == type) {
+            return true;
+        }
+    })
+    let fragment=valid.map((playerData) => {
+        return `
+        <div class="player">
+            <img src="${playerData.image}" alt="">
+            <div class="name">${playerData.name}</div>
+            <div class="strength">${playerData.strength}</div>
+        </div>
+        `;
+    });
+    return fragment.join("");
+}
+
+// Display players in HTML
+const viewPlayers = (players) => {
+    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+}
+
+
+window.onload = () => {
+    viewPlayers(initPlayers(PLAYERS));
+}
